760835970c49b294d645f86933f669233a5836c3
Szpytma <szpytmapawel@hotmail.com>
2023-05-03 16:44:57 +0100

    highscores implemented

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -6,22 +6,32 @@ import {
   limit,
 } from "@firebase/firestore";
 import { firestore } from "../firebase";
+import { useState } from "react";
 
-const scoreRef = collection(firestore, "scores");
-const get10BestScores = query(scoreRef, orderBy("score", "desc"), limit(10));
-const highScores = [];
-getDocs(get10BestScores).then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    highScores.push(doc.data());
-  });
-  console.log(highScores);
-});
 function ScoreBoard() {
+  const [highScores, setHighScores] = useState([]);
+
+  const scoreRef = collection(firestore, "scores");
+  const get10BestScores = query(scoreRef, orderBy("score", "desc"), limit(10));
+
+  const highScoresArr = [];
+
+  getDocs(get10BestScores).then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      highScoresArr.push(doc.data());
+    });
+    setHighScores(highScoresArr);
+  });
+
   return (
     <section id="scores">
       <ol>
         {highScores.map((scores) => {
-          return <li>{scores.username}</li>;
+          return (
+            <li>
+              {scores.username} {scores.score}
+            </li>
+          );
         })}
       </ol>
     </section>
