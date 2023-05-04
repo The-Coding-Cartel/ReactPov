bee296d772b21d3ba8b31e5c617a5be8ff341f22
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-05-04 16:18:06 +0100

    add search by username functionality to scoreboard

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -5,6 +5,7 @@ import {
   orderBy,
   limit,
   getDocs,
+  where,
 } from "@firebase/firestore";
 import { firestore } from "../firebase";
 import moment from "moment";
@@ -14,13 +15,16 @@ function toDate(seconds, nanoseconds) {
     new Date(seconds * 1000 + nanoseconds / 1000000).toLocaleDateString(),
     "D/M/YYYY"
   ).format("DD-MM-YYYY");
+  //   console.log(nanoseconds, seconds);
 
   return yourDate;
 }
 
 function ScoreBoard() {
-  const users = [{ score: 360, username: "Ben Sivyer" }];
   const [highScores, setHighScores] = useState([]);
+  const [searchUser, setSearchUser] = useState("");
+
+  console.log(searchUser);
 
   useEffect(() => {
     const scoreRef = collection(firestore, "scores");
@@ -37,10 +41,37 @@ function ScoreBoard() {
       })
       .catch((error) => console.log(error));
   }, []);
-  console.log(highScores);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const scoreRef = collection(firestore, "scores");
+    const getScoresByUsers = query(
+      scoreRef,
+      where("username", "==", searchUser)
+      //   orderBy("score", "asc")
+    );
+    getDocs(getScoresByUsers)
+      .then((querySnapshot) => {
+        const scoresByUserArr = querySnapshot.docs.map((doc) => doc.data());
+        setHighScores(scoresByUserArr);
+      })
+      .catch((error) => console.log(error));
+  }
 
   return (
     <section id="scores">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="user-search">Search User: </label>
+        <input
+          type="text"
+          id="user-search"
+          value={searchUser}
+          onChange={(event) => {
+            setSearchUser(event.target.value);
+          }}
+        />
+        <button>Go</button>
+      </form>
       <table class="table" id="">
         <thead>
           <tr>
@@ -52,12 +83,13 @@ function ScoreBoard() {
         </thead>
         <tbody>
           {highScores.map((scores, index) => (
-            <tr key={scores.posted_at.nanoseconds}>
+            // <tr key={scores.posted_at.nanoseconds}>
+            <tr>
               <th scope="row">{index + 1}</th>
               <td>{scores.username}</td>
               <td>{scores.score}</td>
               <td>
-                {toDate(scores.posted_at.seconds, scores.posted_at.nanoseconds)}
+                {/* {toDate(scores.posted_at.seconds, scores.posted_at.nanoseconds)} */}
               </td>
             </tr>
           ))}
