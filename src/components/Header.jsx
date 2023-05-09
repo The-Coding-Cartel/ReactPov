98dc21402c2e18c1a281d2a1b4de32a917ea0050
Szpytma <szpytmapawel@hotmail.com>
2023-05-09 14:03:43 +0100

    google auth implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
 import Nav from "./Nav";
+import { auth, googleProvider } from "../firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
 
-function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+function Header({ isLoggedIn, setIsLoggedIn }) {
+  const [user, setUser] = useState();
+
+  function handleGoogleLogin() {
+    signInWithPopup(auth, googleProvider).then(({ user }) => {
+      setUser(user);
+      setIsLoggedIn(true);
+    });
+  }
+
+  function handleSignOut() {
+    signOut(auth).then(setIsLoggedIn(false));
+  }
 
   return (
     <header className="Header">
       <img src="/povmaze-header.png" alt="POVMAZE!"></img>
       <div id="user-icon">User</div>
-      {isLoggedIn && <button>Logout</button>}
-      {!isLoggedIn && <button>Login</button>}
+      {isLoggedIn && <button onClick={() => handleSignOut()}>Logout</button>}
+      {!isLoggedIn && (
+        <button onClick={handleGoogleLogin}>
+          <span>Login with Google</span>
+        </button>
+      )}
+
       <Nav className="Nav" />
     </header>
   );
