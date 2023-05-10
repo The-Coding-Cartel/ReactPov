23e7577b89ce366451e4420cab100291e1420cf6
Pawel Szpytma <56477986+Szpytma@users.noreply.github.com>
2023-05-10 10:07:31 +0100

    Update firebase.js with secrets

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtGtQmF17ZLnumY165DBuFy6GlKqZKv-U",
-  authDomain: "povman-3d5f5.firebaseapp.com",
-  projectId: "povman-3d5f5",
-  storageBucket: "povman-3d5f5.appspot.com",
-  messagingSenderId: "55007516442",
-  appId: "1:55007516442:web:cff06a96b5b0ceba8bbea3",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "povmaze.firebaseapp.com",
+  projectId: "povmaze",
+  storageBucket: "povmaze.appspot.com",
+  messagingSenderId: "435801376958",
+  appId: process.env.REACT_APP_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const firestore = getFirestore(app);
