69a411e3cc6ce35903810a1b3973e81bb5dcd426
Szpytma <szpytmapawel@hotmail.com>
2023-05-10 14:03:26 +0100

    about section updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,23 +3,33 @@ function About() {
     <section id="about">
       <h1>ABOUT!</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-        reiciendis voluptatibus, voluptatem repudiandae doloribus provident
-        eligendi ratione vero non aperiam ducimus magni cupiditate ut quasi
-        nostrum excepturi eius rem nisi? Aperiam voluptates numquam, odit a,
-        aliquid veniam cupiditate, explicabo voluptate nesciunt minima
-        temporibus. Fugiat unde cupiditate quo tenetur eligendi laborum dolor
-        doloribus beatae facere culpa ipsam, rerum, explicabo voluptates enim.
-        Quod laborum, similique aspernatur vitae dolore eum ea accusamus quia
-        error, in nemo pariatur rem provident repellat eius molestiae quas
-        asperiores possimus officia, dicta illum. Repellendus nihil quod eveniet
-        doloremque. Sunt sed accusamus fuga sint modi ipsa eum ea laudantium
-        nisi iusto impedit, animi culpa facilis quibusdam distinctio aspernatur
-        quo eligendi molestiae nam perspiciatis nesciunt dolor. Dolor in
-        voluptatibus adipisci. Repudiandae officia animi exercitationem fugiat,
-        sint amet ratione deleniti nam aliquam, eum possimus doloribus facilis
-        non. Dignissimos possimus corporis cupiditate at repellat dolores iste,
-        id similique maiores placeat, facilis praesentium.
+        POVMAZE is a game built in just 7 days using React, Phaser, and
+        Firebase. The project began as a 2D version of Pacman, but over the
+        course of the week, the team expanded it into a fully-realized 3D maze
+        runner with raycasting.
+      </p>
+      <p>
+        The game is designed to be played from a first-person point of view,
+        immersing the player in the game world. The objective is to navigate
+        through the maze, collecting items and avoiding obstacles.
+      </p>
+      <p>
+        One of the key features of the game is the use of raycasting, a
+        technique that simulates the behavior of light rays to create 3D
+        graphics in real-time. This allows for realistic rendering of the maze
+        and its surroundings, and creates a more immersive experience for the
+        player.
+      </p>
+      <p>
+        Throughout the development process, the team faced numerous challenges,
+        from optimizing performance to fine-tuning the game mechanics. However,
+        the end result is a polished and engaging game that demonstrates the
+        power of modern web technologies.
+      </p>
+      <p>
+        POVMAZE is a testament to the creativity and ingenuity of the team
+        behind it, who were able to build a complex and engaging game in just 7
+        days.
       </p>
     </section>
   );
